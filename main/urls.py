8a3f9da588751d709666e09d5acf8a9f857f11f5
Ridu <49078437+redham5@users.noreply.github.com>
2023-08-18 21:57:00 +0600

    Login

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,15 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
+from .forms import LoginForm
 
 app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='main/login.html', authentication_form=LoginForm), name='login'),
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
